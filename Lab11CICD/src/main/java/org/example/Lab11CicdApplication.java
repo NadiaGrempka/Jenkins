16abfd392b852a00ef7eb4ecc35f10d07b41554b
Nadia <130447047+NadiaGrempka@users.noreply.github.com>
2025-05-22 19:06:28 +0200

    Root project
    adds main project files

---
+++ Lab11CICD/src/main/java/org/example/Lab11CicdApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab11CicdApplication.class, args);
+    }
+
+}
